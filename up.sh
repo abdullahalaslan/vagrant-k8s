516fd447b68d08827c107957a05d929d69fc6230
Baturay Ozcan <baturayozcan@gmail.com>
2020-11-25 01:03:32 +0300

    k8s yaml path updated

--- up.sh
+++ up.sh
@@ -6,7 +6,7 @@
 #curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 && sudo install skaffold /usr/local/bin/ -y
 vagrant up
 export DB_PASSWORD=`pwgen Bs1 12`
-kubectl apply -f mysql-deployment.yaml
-kubectl apply -f mysql-service.yaml
-kubectl apply -f registry-deployment.yaml
-kubectl apply -f registry-service.yaml
+kubectl apply -f k8s/mysql-deployment.yaml
+kubectl apply -f k8s/mysql-service.yaml
+kubectl apply -f k8s/registry-deployment.yaml
+kubectl apply -f k8s/registry-service.yaml
