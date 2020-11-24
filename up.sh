f67d5c4e8e9a1727a7e42785f7854d1561172be3
Baturay Ozcan <baturayozcan@gmail.com>
2020-11-25 00:20:26 +0300

    skaffold installation step added

--- up.sh
+++ up.sh
@@ -1,7 +1,8 @@
-sudo apt-get update
-sudo apt-get install virtualbox -y
-sudo apt-get install vagrant -y
-sudo apt-get install ansible -y
-sudo apt-get install pwgen -y
+#sudo apt-get update
+#sudo apt-get install virtualbox -y
+#sudo apt-get install vagrant -y
+#sudo apt-get install ansible -y
+#sudo apt-get install pwgen -y
+#curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 && sudo install skaffold /usr/local/bin/ -y
 vagrant up
 export DB_PASSWORD=`pwgen Bs1 12`
