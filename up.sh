3a7724647f5d7a16b1478596727d57fb73718df2
Baturay Ozcan <baturayozcan@gmail.com>
2020-11-25 00:47:00 +0300

    registry service step added

--- up.sh
+++ up.sh
@@ -9,3 +9,4 @@ export DB_PASSWORD=`pwgen Bs1 12`
 kubectl apply -f mysql-deployment.yaml
 kubectl apply -f mysql-service.yaml
 kubectl apply -f registry-deployment.yaml
+kubectl apply -f registry-service.yaml
