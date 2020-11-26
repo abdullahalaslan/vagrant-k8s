1ae11e81cb32910219318d746bb9d4dba28c01e2
Baturay Ozcan <baturayozcan@gmail.com>
2020-11-26 22:18:41 +0300

    skaffold trigger changed to polling

--- ansible/roles/launch/files/launch.sh
+++ ansible/roles/launch/files/launch.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 export DB_PASSWORD=`pwgen -Bs1 12`
 kubectl apply -f /vagrant/manifests/mysql-deployment.yaml
 kubectl apply -f /vagrant/manifests/mysql-service.yaml
 kubectl apply -f /vagrant/manifests/app-service.yaml
 cd /home/vagrant/hello-python
-skaffold dev </dev/null &>/dev/null &
+skaffold dev --trigger='polling' --watch-poll-interval=1000
