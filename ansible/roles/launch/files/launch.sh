f72070ceea516084d6f643e8f4a200c0a99ea9c5
Baturay Ozcan <baturayozcan@gmail.com>
2020-11-26 19:59:59 +0300

    apply app service yaml

--- ansible/roles/launch/files/launch.sh
+++ ansible/roles/launch/files/launch.sh
@@ -1,5 +1,6 @@
 export DB_PASSWORD=`pwgen -Bs1 12`
 kubectl apply -f /vagrant/manifests/mysql-deployment.yaml
 kubectl apply -f /vagrant/manifests/mysql-service.yaml
+kubectl apply -f /vagrant/manifests/app-service.yaml
 cd /home/vagrant/hello-python
-skaffold run
+skaffold dev
