42ee39b919a0b2327954dccbfe305d6094b87206
Baturay Ozcan <baturayozcan@gmail.com>
2020-11-26 21:07:09 +0300

    run skaffold in background

--- ansible/roles/launch/files/launch.sh
+++ ansible/roles/launch/files/launch.sh
@@ -3,4 +3,4 @@ kubectl apply -f /vagrant/manifests/mysql-deployment.yaml
 kubectl apply -f /vagrant/manifests/mysql-service.yaml
 kubectl apply -f /vagrant/manifests/app-service.yaml
 cd /home/vagrant/hello-python
-skaffold dev
+skaffold dev </dev/null &>/dev/null &
