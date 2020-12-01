841fc5642f5715c8e382eca478126004ffe71670
Baturay Ozcan <baturayozcan@gmail.com>
2020-12-01 16:12:24 +0300

    skaffold dev changed to skaffold run

--- ansible/roles/launch/files/launch.sh
+++ ansible/roles/launch/files/launch.sh
@@ -5,4 +5,5 @@ kubectl apply -f /vagrant/manifests/mysql-deployment.yaml
 kubectl apply -f /vagrant/manifests/mysql-service.yaml
 kubectl apply -f /vagrant/manifests/app-service.yaml
 cd /home/vagrant/hello-python
-skaffold dev --trigger='polling' --watch-poll-interval=1000
+#skaffold dev --trigger='polling' --watch-poll-interval=1000
+skaffold run
